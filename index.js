414240bb59871ab2635415217f9d28fc16cfe9f8
ashutyson <ashwin.katkar@hotmail.com>
2018-03-31 20:26:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ restService.post("/echo", function(req, res) {
       return res.json({
           speech: speech,
           displayText: speech,
-          source: "webhook-echo-sample"
+          source: "webhook-echo-sample3"
       });
   
   });
